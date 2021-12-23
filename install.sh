5c6066645e348f60fcada8e45dd1b7122f8eb101
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-23 15:11:43 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install cmake
+sudo apt-get install gcc g++
+sudo apt-get install python-dev python-numpy
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libpng-dev
+sudo apt-get install libjpeg-dev
+sudo apt-get install libopenexr-dev
+sudo apt-get install libtiff-dev
+sudo apt-get install libwebp-dev
+ sudo apt-get install git
+ https://github.com/opencv/opencv.git
