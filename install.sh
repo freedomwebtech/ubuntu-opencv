cf5f35471ff8442da854185ad52c567ae0184649
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-12-24 09:04:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
-sudo apt-get install cmake
-sudo apt-get install gcc g++
-sudo apt-get install python-dev python-numpy
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
-sudo apt-get install libgtk2.0-dev
-sudo apt-get install libgtk-3-dev
-sudo apt-get install libpng-dev
-sudo apt-get install libjpeg-dev
-sudo apt-get install libopenexr-dev
-sudo apt-get install libtiff-dev
-sudo apt-get install libwebp-dev
-sudo apt-get install git
-sudo git clone https://github.com/opencv/opencv.git
-cd opencv
-mkdir build
-cd build
-cmake ../
-make
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-22-dev -y
+
+mkdir ~/opencv_build && cd ~/opencv_build
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+cd ~/opencv_build/opencv
+mkdir build && cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
+make -j8
 sudo make install
+pkg-config --modversion opencv4
